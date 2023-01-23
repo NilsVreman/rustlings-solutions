29af91ea06c90a12fc378fd66af2eb8fb93a4156
NilsVreman <nils.vreman@gmail.com>
2023-01-23 11:15:01 +0100

    Added my solutions to the different rustlings exercises

---
+++ clippy/target/debug/deps/clippy3-3ac35682ada9ee27.d
@@ -0,0 +1,7 @@
+/local/home/nilsv/PhD/projects/rustlings/exercises/clippy/target/debug/deps/clippy3-3ac35682ada9ee27.rmeta: clippy3.rs
+
+/local/home/nilsv/PhD/projects/rustlings/exercises/clippy/target/debug/deps/clippy3-3ac35682ada9ee27.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
